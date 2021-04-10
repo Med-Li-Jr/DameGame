dfe38dc8e3e97787bcfbcc2b3aac9fb6e4ee8485
med-li-jr <li.med.jr@gmail.com>
2021-04-10 17:13:16 +0100

    show dash with js

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+
+let dashboard = document.getElementById("dashboard")
 let dash = [
-    [0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0]
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0]
 ]
-
-let allRoad = document.getElementsByClassName("myBox");
-let len = allRoad.length
-let k = 0, j=0;
-for(let i = 0; i < len; i++){
-    let element = allRoad[i];
-    j++;
-    if(j==8){
-        j = 0;
-        k++;
+function showDash() {
+    let i = 1;
+    for (let row of dash) {
+        let j = 1;
+        for (let col of row) {
+            if ((i%2 != 0 && j%2 != 0) ||
+            (i%2 == 0 && j%2 == 0))            
+                createPion(col, "white")
+            else
+                createPion(col, "black")
+            j++;
+        }
+        i++;
     }
-    element.addEventListener("click", elementCliked, false);
-    console.log(element);
-
+}
+function createPion(col, bkgColor) {
+    let div = document.createElement("div");
+    div.classList.add("myBox");
+    let img = document.createElement("img")
+    div.classList.add("box-" + bkgColor)
+    if (col === 1) {
+        img.src = "./checker1.png";
+    }
+    else if (col === -1) {
+        img.src = "./checker2.png";
+    }
+    else {
+        img.src = "./transparent.png";
+    }
+    div.appendChild(img);
+    dashboard.appendChild(div);
 }
 
-function elementCliked(x,y){
-    alert(x + " // " + y)
-
-}
+showDash()
